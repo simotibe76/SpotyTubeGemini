94b53c1bd1399601d54bed5571bfc565dab6a9fe
simotibe76 <simotibe@gmail.com>
2025-08-09 10:57:19 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Welcome from './Welcome.jsx' // Importa il nuovo componente Welcome
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Welcome /> {/* Renderizza il componente Welcome */}
   </StrictMode>,
 )
